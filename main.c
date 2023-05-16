406655565159a61fce2bf85f772e5e12744db753
Roko Sirovic <rokosirovic123@gmail.com>
2023-05-16 15:57:16 +0200

    ajsad

--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-  Function Declarations for builtin shell commands:
- */
+
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
